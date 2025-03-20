49ab90b885c4435cdd787306bc8e1bf46ae4b1f9
Abdulrahman Baiasy <baiasyabdulrahman@gmail.com>
2025-03-20 11:34:55 +0700

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>
+);
