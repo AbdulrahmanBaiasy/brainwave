e1d0e58662e958612bdcfc0f982d991bfa3d62eb
Abdulrahman Baiasy <baiasyabdulrahman@gmail.com>
2025-03-23 22:28:35 +0700

    Add Pricing, Roadmap, Footer

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -2,7 +2,6 @@ import Section from "./Section";
 import { collabApps, collabContent, collabText } from "../constants";
 import Button from "./Button";
 import { brainwaveSymbol, check } from "../assets";
-import { LeftCurve, RightCurve } from "./design/Collaboration";
 const Collaboration = () => {
   return (
     <Section crosses>
